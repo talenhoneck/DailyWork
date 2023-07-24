d9e52e759c4929d826682cff60487809bc3b224c
Talen Honeck <talenhoneck@gmail.com>
2023-07-24 14:20:41 -0400

    Initial Commit

---
+++ Code Wars/Template/Answer.js
@@ -0,0 +1,10 @@
+// Title: 
+// Instructions: 
+// Test Code Command: npx mocha Problem.test.js
+
+  function smash(words) {
+    return words.join(" ");
+  }
+  
+  module.exports = smash;
+  
---
+++ Code Wars/Template/Problem.test.js
@@ -0,0 +1,15 @@
+// Test cases using Chai assertions
+
+
+// Importing Chai testing framework and a custom function.
+const chai = require("chai");
+const assert = chai.assert;
+const smash = require("./Answer");
+
+// Configuring Chai (a library used for testing in JavaScript) to show complete and unshortened error messages when an assertion fails.
+chai.config.truncateThreshold = 0;
+
+// Test cases
+// Add this:     console.log("Result:", result); // Output the result to the console
+
+
